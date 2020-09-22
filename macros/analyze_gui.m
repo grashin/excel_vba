e8e6a303612fa09c0c68751d7c09a6cc833a2fea
grashin <grashin2011@gmail.com>
2020-09-23 00:54:15 +0300

    add files

---
+++ macros/analyze_gui.m
@@ -0,0 +1,202 @@
+function varargout = untitled1(varargin)
+% UNTITLED1 MATLAB code for untitled1.fig
+%      UNTITLED1, by itself, creates a new UNTITLED1 or raises the existing
+%      singleton*.
+%
+%      H = UNTITLED1 returns the handle to a new UNTITLED1 or the handle to
+%      the existing singleton*.
+%
+%      UNTITLED1('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in UNTITLED1.M with the given input arguments.
+%
+%      UNTITLED1('Property','Value',...) creates a new UNTITLED1 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before untitled1_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to untitled1_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help untitled1
+
+% Last Modified by GUIDE v2.5 20-Dec-2018 21:25:20
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @untitled1_OpeningFcn, ...
+                   'gui_OutputFcn',  @untitled1_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before untitled1 is made visible.
+function untitled1_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to untitled1 (see VARARGIN)
+
+% Choose default command line output for untitled1
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes untitled1 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = untitled1_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes during object creation, after setting all properties.
+function uibuttongroup1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to uibuttongroup1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+
+% --- Executes on button press in pushbutton.
+function pushbutton_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+cla reset;
+filename = 'Данные по 20 пробам';
+axes(handles.axes1);
+sheet = 1;
+table=['E','F','G','I','J','K','M','N','O','P','Q','S','T','U','V','W','X','Y','Z','AA','AB','AC','AE','AF','AG','AH'];
+x=get(handles.parameter_1, 'String');
+x=str2double(x);
+y=get(handles.parameter_2, 'String');
+y=str2double(y);
+column1=table(x);
+column2=table(y);
+Column1_1 = xlsread(filename,sheet,[column1 '3:' column1 '6']);
+Column1_2 = xlsread(filename,sheet,[column1 '8:' column1 '16']);
+Column1_3 = xlsread(filename,sheet,[column1 '18:' column1 '21']);
+Column1_4 = xlsread(filename,sheet,[column1 '23:' column1 '25']);
+Column2_1 = xlsread(filename,sheet,[column2 '3:' column2 '6']);
+Column2_2 = xlsread(filename,sheet,[column2 '8:' column2 '16']);
+Column2_3 = xlsread(filename,sheet,[column2 '18:' column2 '21']);
+Column2_4 = xlsread(filename,sheet,[column2 '23:' column2 '25']);
+
+
+scatter(Column1_1,Column2_1,'g','LineWidth',2); grid;
+hold on;
+scatter(Column1_2,Column2_2,'r','LineWidth',2); hold on;
+
+scatter(Column1_3,Column2_3,'k','LineWidth',2);hold on;
+
+scatter(Column1_4,Column2_4,'b','LineWidth',2); hold on;
+
+
+
+
+
+% --- Executes on button press in checkbox1.
+function checkbox1_Callback(hObject, eventdata, handles)
+
+% hObject    handle to checkbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of checkbox1
+
+
+% --- Executes on button press in checkbox2.
+function checkbox2_Callback(hObject, eventdata, handles)
+% hObject    handle to checkbox2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of checkbox2
+
+
+% --- Executes on button press in checkbox3.
+function checkbox3_Callback(hObject, eventdata, handles)
+% hObject    handle to checkbox3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of checkbox3
+
+
+
+function parameter_1_Callback(hObject, eventdata, handles)
+% hObject    handle to parameter_1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of parameter_1 as text
+%        str2double(get(hObject,'String')) returns contents of parameter_1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function parameter_1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to parameter_1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function parameter_2_Callback(hObject, eventdata, handles)
+% hObject    handle to parameter_2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of parameter_2 as text
+%        str2double(get(hObject,'String')) returns contents of parameter_2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function parameter_2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to parameter_2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes during object creation, after setting all properties.
+function axes1_CreateFcn(hObject, eventdata, handles)
+
+% hObject    handle to axes1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: place code in OpeningFcn to populate axes1
